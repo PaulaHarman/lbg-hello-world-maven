30ba8c0c0a0ad1d58e0a3808490ebb36be98bd5b
paula harman <paula_harman@outlook.com>
2023-04-25 12:24:23 +0100

    I have added an extra line between 15 and 15 to say hello to somebody else

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -13,6 +13,7 @@ public class App
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
+        system.out.printIn(sayHelloToSomeone("Jonathan")); 
         System.out.println(sayGoodbye());
     }
 
